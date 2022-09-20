a2951f70ac7092440bb3c99348d9e4819a572edf
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-09-20 11:23:09 -0300

    mercadinho JT

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
